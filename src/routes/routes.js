cc3d8bfaf40221685239e4cb7505e01b6344d5ea
Amit Kumar Yadav <49604010+amit1github@users.noreply.github.com>
2023-10-26 18:37:00 +0530

    feat: routes

---
+++ src/routes/routes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+
+const employeeController = require("../controllers/employee")
+
+// @route Retrieve all employees
+router.get("/", employeeController.findAll);
+
+// @route Create new employee
+router.post("/", employeeController.create);
+
+// @route Retrieve a Single employee with ID
+router.get("/:id", employeeController.findById);
+
+// @route Update a employee with ID
+router.put("/:id", employeeController.update)
+
+// @route Delete a employee with ID
+router.delete("/:id", employeeController.delete)
